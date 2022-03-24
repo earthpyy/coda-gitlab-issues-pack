5c4671c8f93b60cd38689aaacfd5ffb55aefc9e2
Preeti Yuankrathok <preetisatit@gmail.com>
2022-03-24 23:05:40 +0700

    Add GitLab oauth2

--- pack.ts
+++ pack.ts
@@ -1,3 +1,9 @@
 import * as coda from '@codahq/packs-sdk'
 
 export const pack = coda.newPack()
+
+pack.setUserAuthentication({
+  type: coda.AuthenticationType.OAuth2,
+  authorizationUrl: 'https://gitlab.com/oauth/authorize',
+  tokenUrl: 'https://gitlab.com/oauth/token',
+})
